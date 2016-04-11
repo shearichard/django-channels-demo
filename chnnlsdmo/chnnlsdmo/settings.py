70d8cdf263ff97c48f77d492c74261c46d58efd8
Richard Shea <rshea@thecubagroup.com>
2016-04-12 11:29:42 +1200

    This is a version which gets its bootstrap assets from a CDN and it works locally

--- chnnlsdmo/chnnlsdmo/settings.py
+++ chnnlsdmo/chnnlsdmo/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,3 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+print("**************************************")
+print(STATIC_ROOT)
+print("**************************************")
+print(BASE_DIR)
+print("**************************************")
+STATICFILES_DIRS = [
+    "/media/poloextra/dev/django-channels-demo/chnnlsdmo/chnnlsdmo/static"
+]
