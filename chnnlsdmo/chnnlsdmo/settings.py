c661954463aeb84416b38365534d5b58c3f19422
Richard Shea <rshea@thecubagroup.com>
2016-04-12 16:57:53 +1200

    Installed whitenose (https://warehouse.python.org/project/whitenoise/)

--- chnnlsdmo/chnnlsdmo/settings.py
+++ chnnlsdmo/chnnlsdmo/settings.py
@@ -120,6 +120,9 @@ USE_L10N = True
 USE_TZ = True
 
 
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
