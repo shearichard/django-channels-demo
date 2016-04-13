e6c9f02cab92e7aa63f7fb139df4e8ef68de36c7
Richard Shea <rshea@thecubagroup.com>
2016-04-13 14:24:36 +1200

    Corrected error in wsgi.py

--- chnnlsdmo/chnnlsdmo/wsgi.py
+++ chnnlsdmo/chnnlsdmo/wsgi.py
@@ -12,8 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chnnlsdmo.settings")
-
 if 'DYNO' in os.environ:
     debug = False
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chnnlsdmo.settings_heroku")
