5f02c491a56ca9a06f49417dfddca09e5b1fb5dc
Richard Shea <rshea@thecubagroup.com>
2016-04-13 13:50:27 +1200

    Making Postgres work on heroku

--- chnnlsdmo/chnnlsdmo/settings_heroku.py
+++ chnnlsdmo/chnnlsdmo/settings_heroku.py
@@ -16,3 +16,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
